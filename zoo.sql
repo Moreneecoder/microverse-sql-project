a9df83f8d7cd2fe3f13bbea1573ce6bb29974379
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-16 22:48:14 +0100

    Add answer for zoo project question 20

--- zoo.sql
+++ zoo.sql
@@ -46,3 +46,5 @@ SELECT name FROM world WHERE name LIKE '%United%'
 SELECT name, population, area FROM world WHERE area > 3000000 OR  population > 250000000
 
 SELECT name, population, area FROM world WHERE (population > 250000000 AND area <= 3000000) OR (population <= 250000000 AND area > 3000000)
+
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) AS gdp FROM world WHERE continent = 'South America'
