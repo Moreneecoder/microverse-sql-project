16510227ebd9c32975e3490e9836629ec73105de
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-16 23:07:39 +0100

    Add answer for zoo project question 22

--- zoo.sql
+++ zoo.sql
@@ -50,3 +50,5 @@ SELECT name, population, area FROM world WHERE (population > 250000000 AND area 
 SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) AS gdp FROM world WHERE continent = 'South America'
 
 SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp >= 1000000000000
+
+SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital)
