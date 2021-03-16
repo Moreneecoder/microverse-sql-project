4bcb2274004c99cb937f4d0999a43a400d716216
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-16 22:33:08 +0100

    Add answer for zoo project question 17

--- zoo.sql
+++ zoo.sql
@@ -40,3 +40,5 @@ SELECT name, gdp/population FROM world WHERE population >= 200000000
 SELECT name, (population/1000000) AS population FROM world WHERE continent = 'South America'
 
 SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy')
+
+SELECT name FROM world WHERE name LIKE '%United%'
