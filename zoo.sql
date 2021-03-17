87c31a53b09c49cae5b00f0148514608563d6d56
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-17 12:32:45 +0100

    Add answers to SELECT from SELECT  quiz

--- zoo.sql
+++ zoo.sql
@@ -132,3 +132,26 @@ Literature	1
 Medicine	2
 Peace	1
 Physics	1
+
+SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
+
+SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
+
+France
+Germany
+Russia
+Turkey
+
+SELECT name FROM bbc
+ WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
+
+SELECT name FROM bbc
+ WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+   AND population > (SELECT population FROM bbc WHERE name='Denmark')
+
+Bangladesh
+India
+Pakistan
