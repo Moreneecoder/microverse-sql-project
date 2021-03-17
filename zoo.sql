b6f6d0d3fb115ba6d15fbbc6017f1d7dc8b4dfb9
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-17 12:05:33 +0100

    Add answers to SELECT from nobel quiz

--- zoo.sql
+++ zoo.sql
@@ -98,3 +98,37 @@ SELECT * FROM nobel WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemis
 SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
 
 SELECT * FROM nobel WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004
+
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+
+SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+
+Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet
+
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1
