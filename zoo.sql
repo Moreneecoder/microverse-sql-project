4bf3e53bb57b089459ba0ca342d6c588a5985915
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-17 12:02:36 +0100

    Add answers to SELECT from nobel tutorials

--- zoo.sql
+++ zoo.sql
@@ -78,4 +78,23 @@ SELECT name FROM world
 Brazil
 Colombia
 
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein'
+
+SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000
+
+SELECT * FROM nobel WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+
+SELECT winner FROM nobel WHERE winner LIKE 'John%'
+
+SELECT * FROM nobel WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984
+
+SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+SELECT * FROM nobel WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004
