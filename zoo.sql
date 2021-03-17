bcf736cfb13d5dfc51eb79d053f03ba0e0baa170
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-17 13:28:25 +0100

    Add answers to MORE JOINS  quiz

--- zoo.sql
+++ zoo.sql
@@ -205,3 +205,33 @@ Netherlands	2
 Poland	2
 Republic of Ireland	1
 Ukraine	2
+
+SELECT name
+  FROM actor INNER JOIN movie ON actor.id = director
+ WHERE gross < budget
+
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid
+
+SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+ WHERE name LIKE 'John %'
+ GROUP BY name ORDER BY 2 DESC
+
+Crocodile Dundee
+Crocodile Dundee in Los Angeles
+Flipper
+Lightning Jack
+
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+
+link the director column in movies with the primary key in actor
+connect the primary keys of movie and actor via the casting table
+
+A Bronx Tale	1993
+Bang the Drum Slowly	1973
+Limitless	2011
